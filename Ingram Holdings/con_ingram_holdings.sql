2161c5bf22448ef55b5c59743fff0e8c074c561a
Jeremy Goldstein <jgoldstein@minlib.net>
2026-01-08 13:44:29 -0500

    ingram holdings query updates

--- Ingram Holdings/con_ingram_holdings.sql
+++ Ingram Holdings/con_ingram_holdings.sql
@@ -1,78 +1,70 @@
 --print items and orders for Concord
 
-DROP TABLE IF EXISTS con_bibs;
+WITH not_multi_volume AS (
+  SELECT l.bib_record_id
+  
+  FROM sierra_view.bib_record_item_record_link l
+  LEFT JOIN sierra_view.varfield v
+    ON l.item_record_id = v.record_id
+    AND v.varfield_type_code = 'v'
+    
+  GROUP BY 1
+  HAVING COUNT(l.item_record_id) FILTER(WHERE v.field_content IS NOT NULL) = 0
+),
 
-CREATE TEMP TABLE con_bibs AS
+con_bibs AS (
+  SELECT
+    DISTINCT inner_query.bib_record_id
+  FROM (
+    SELECT
+      l.bib_record_id
+
+      FROM not_multi_volume nmv
+      JOIN sierra_view.bib_record_item_record_link l
+        ON nmv.bib_record_id = l.bib_record_id
+      JOIN sierra_view.item_record i
+        ON l.item_record_id = i.id
+
+      WHERE i.location_code ~ '^co'
+
+    UNION
+
+    SELECT
+      ol.bib_record_id
+    
+    FROM not_multi_volume nmv
+    JOIN sierra_view.bib_record_order_record_link ol
+      ON nmv.bib_record_id = ol.bib_record_id
+    JOIN sierra_view.order_record o
+      ON ol.order_record_id = o.id
+
+    WHERE o.accounting_unit_code_num = '8'
+  )inner_query 
+),
+
+isbn AS (
+SELECT
+  s.record_id,
+  COALESCE(STRING_AGG(DISTINCT SUBSTRING(s.content FROM '^\d{9,12}[\d|X]'),'|'),'') AS "isbns"
+  FROM con_bibs b
+  JOIN sierra_view.subfield s
+    ON b.bib_record_id = s.record_id
+	 AND s.marc_tag = '020'
+	 AND s.tag = 'a'
+  GROUP BY 1
+)
+
 
 SELECT
-b.bib_record_id
+  i.record_id,
+  COALESCE(SUBSTRING(o.content FROM '[0-9]+'),'') AS "001",
+  i.isbns AS "020"
 
-FROM
-sierra_view.bib_record_property b
-LEFT JOIN
-sierra_view.bib_record_item_record_link l
-ON
-b.bib_record_id = l.bib_record_id
-LEFT JOIN
-sierra_view.item_record i
-ON
-l.item_record_id = i.id  AND i.location_code ~ '^co' --AND i.item_status_code NOT IN ('w','$')
-LEFT JOIN
-sierra_view.bib_record_order_record_link ol
-ON
-b.bib_record_id = ol.bib_record_id
-LEFT JOIN
-sierra_view.order_record o
-ON
-ol.order_record_id = o.id AND o.accounting_unit_code_num = '8'
-LEFT JOIN
-sierra_view.varfield v
-ON
-i.id = v.record_id AND v.varfield_type_code = 'v'
+FROM isbn i
+JOIN sierra_view.bib_record_property b
+  ON i.record_id = b.bib_record_id 
+LEFT JOIN sierra_view.subfield o
+  ON i.record_id = o.record_id
+  AND o.marc_tag = '001'
 
-WHERE i.id IS NOT NULL OR o.id IS NOT NULL
-GROUP BY 1
-HAVING
-COUNT(i.id) FILTER(WHERE v.field_content IS NOT NULL) = 0
-;
-
-DROP TABLE IF EXISTS isbn;
-
-CREATE TEMP TABLE isbn AS
-
-SELECT
-s.record_id,
-COALESCE(STRING_AGG(DISTINCT SUBSTRING(s.content FROM '^\d{9,12}[\d|X]'),'|'),'') AS "isbns"
-FROM
-con_bibs b
-JOIN
-sierra_view.subfield s
-ON
-b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
-GROUP BY 1
-;
-
-SELECT
-i.record_id,
---trying to use phase entry instead of subfield for 001
-COALESCE(SUBSTRING(o.content FROM '[0-9]+'),'') AS "001",
-i.isbns AS "020"--,
---COALESCE(STRING_AGG(DISTINCT SUBSTRING(s3.content FROM '^\d+'),'|'),'') AS "024"
-
-FROM
-isbn i
-JOIN
-sierra_view.bib_record_property b
-ON
-i.record_id = b.bib_record_id AND b.material_code IN ('2','f','9','a','e','o','p','t')
-LEFT JOIN
-sierra_view.subfield o
-ON
-i.record_id = o.record_id AND o.marc_tag = '001'
-/*LEFT JOIN
-sierra_view.subfield s3
-ON
-bi.bib_record_id = s3.record_id AND s3.marc_tag = '024' AND s3.tag = 'a'
-
-WHERE 
-s3.content IS NOT NULL*/
+WHERE b.material_code IN ('2','f','9','a','e','o','p','t')
--- Ingram Holdings/fpl_ingram_holdings.sql
+++ Ingram Holdings/fpl_ingram_holdings.sql
@@ -1,78 +1,71 @@
 --print items and orders for Framingham, not included withdrawn or lost & paid items
 
-DROP TABLE IF EXISTS fpl_bibs;
+WITH not_multi_volume AS (
+  SELECT l.bib_record_id
+  
+  FROM sierra_view.bib_record_item_record_link l
+  LEFT JOIN sierra_view.varfield v
+    ON l.item_record_id = v.record_id
+    AND v.varfield_type_code = 'v'
+    
+  GROUP BY 1
+  HAVING COUNT(l.item_record_id) FILTER(WHERE v.field_content IS NOT NULL) = 0
+),
 
-CREATE TEMP TABLE fpl_bibs AS
+fpl_bibs AS (
+  SELECT
+    DISTINCT inner_query.bib_record_id
+  FROM (
+    SELECT
+      l.bib_record_id
 
-SELECT
-b.bib_record_id
+      FROM not_multi_volume nmv
+      JOIN sierra_view.bib_record_item_record_link l
+        ON nmv.bib_record_id = l.bib_record_id
+      JOIN sierra_view.item_record i
+        ON l.item_record_id = i.id
 
-FROM
-sierra_view.bib_record_property b
-LEFT JOIN
-sierra_view.bib_record_item_record_link l
-ON
-b.bib_record_id = l.bib_record_id
-LEFT JOIN
-sierra_view.item_record i
-ON
-l.item_record_id = i.id  AND i.location_code ~ '^fp' AND i.item_status_code NOT IN ('w','$')
-LEFT JOIN
-sierra_view.bib_record_order_record_link ol
-ON
-b.bib_record_id = ol.bib_record_id
-LEFT JOIN
-sierra_view.order_record o
-ON
-ol.order_record_id = o.id AND o.accounting_unit_code_num = '12'
-LEFT JOIN
-sierra_view.varfield v
-ON
-i.id = v.record_id AND v.varfield_type_code = 'v'
+      WHERE i.location_code ~ '^fp'
+        AND i.item_status_code NOT IN ('w','$')
 
-WHERE i.id IS NOT NULL OR o.id IS NOT NULL
-GROUP BY 1
-HAVING
-COUNT(i.id) FILTER(WHERE v.field_content IS NOT NULL) = 0
-;
+    UNION
 
-DROP TABLE IF EXISTS isbn;
+    SELECT
+      ol.bib_record_id
+    
+    FROM not_multi_volume nmv
+    JOIN sierra_view.bib_record_order_record_link ol
+      ON nmv.bib_record_id = ol.bib_record_id
+    JOIN sierra_view.order_record o
+      ON ol.order_record_id = o.id
 
-CREATE TEMP TABLE isbn AS
+    WHERE o.accounting_unit_code_num = '12'
+  )inner_query 
+),
 
-SELECT
-s.record_id,
-COALESCE(STRING_AGG(DISTINCT SUBSTRING(s.content FROM '^\d{9,12}[\d|X]'),'|'),'') AS "isbns"
-FROM
-fpl_bibs b
-JOIN
-sierra_view.subfield s
-ON
-b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
-GROUP BY 1
-;
+isbn AS (
 
 SELECT
-i.record_id,
---trying to use phase entry instead of subfield for 001
-COALESCE(SUBSTRING(o.content FROM '[0-9]+'),'') AS "001",
-i.isbns AS "020"--,
---COALESCE(STRING_AGG(DISTINCT SUBSTRING(s3.content FROM '^\d+'),'|'),'') AS "024"
+  s.record_id,
+  COALESCE(STRING_AGG(DISTINCT SUBSTRING(s.content FROM '^\d{9,12}[\d|X]'),'|'),'') AS "isbns"
+  FROM fpl_bibs b
+  JOIN sierra_view.subfield s
+    ON b.bib_record_id = s.record_id
+	 AND s.marc_tag = '020'
+	 AND s.tag = 'a'
+  GROUP BY 1
+)
 
-FROM
-isbn i
-JOIN
-sierra_view.bib_record_property b
-ON
-i.record_id = b.bib_record_id AND b.material_code IN ('2','f','9','a','e','o','p','t')
-LEFT JOIN
-sierra_view.subfield o
-ON
-i.record_id = o.record_id AND o.marc_tag = '001'
-/*LEFT JOIN
-sierra_view.subfield s3
-ON
-bi.bib_record_id = s3.record_id AND s3.marc_tag = '024' AND s3.tag = 'a'
+SELECT
+  i.record_id,
+  COALESCE(SUBSTRING(o.content FROM '[0-9]+'),'') AS "001",
+  i.isbns AS "020"
 
-WHERE 
-s3.content IS NOT NULL*/
+FROM isbn i
+JOIN sierra_view.bib_record_property b
+  ON i.record_id = b.bib_record_id 
+LEFT JOIN sierra_view.subfield o
+  ON i.record_id = o.record_id
+  AND o.marc_tag = '001'
+
+WHERE b.material_code IN ('2','f','9','a','e','o','p','t')
