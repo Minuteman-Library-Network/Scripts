9109fddcab2a8febf7a6a5b784d851aac6ee238c
Jeremy Goldstein <jgoldstein@minlib.net>
2026-02-16 11:23:12 -0500

    correct holds error for libraryiq extracts
    Holds and unfilled holds queries had been missing cases in which a hold was placed on a bib lacking items or volumes.  Queries have been rewritten to address this error.

--- LibraryIQ/LibraryIQ.py
+++ LibraryIQ/LibraryIQ.py
@@ -318,14 +318,28 @@ def main():
       COUNT(DISTINCT h.id) AS "Number of requests"
   
     FROM sierra_view.hold h
-    --address both item and bib level holds
+    --address item level holds
     JOIN sierra_view.bib_record_item_record_link l
-      ON h.record_id = l.bib_record_id
-      OR h.record_id = l.item_record_id
+      ON h.record_id = l.item_record_id
     JOIN sierra_view.record_metadata rm
       ON l.bib_record_id = rm.id
 
-    WHERE h.expires_gmt::DATE > CURRENT_DATE
+    WHERE h.expires_gmt::DATE > CURRENT_DATE OR h.expires_gmt IS NULL
+    GROUP BY 1,2
+    
+    UNION
+    
+    SELECT
+      DISTINCT rm.record_type_code||rm.record_num AS "BibNum",
+      SUBSTRING(h.pickup_location_code,1,3) AS "BranchID",
+      COUNT(DISTINCT h.id) AS "Number of requests"
+  
+    FROM sierra_view.hold h
+    --address bib level holds
+    JOIN sierra_view.record_metadata rm
+      ON h.record_id = rm.id AND rm.record_type_code = 'b'
+
+    WHERE h.expires_gmt::DATE > CURRENT_DATE OR h.expires_gmt IS NULL
     GROUP BY 1,2
     """
 
@@ -490,22 +504,48 @@ def main():
     FROM sierra_view.hold h
     /*Using or in the join to reconcile both bib holds and item holds to a bib record*/
     JOIN sierra_view.bib_record_item_record_link li
-      ON h.record_id = li.bib_record_id
-      OR h.record_id = li.item_record_id
+      ON h.record_id = li.item_record_id
     JOIN sierra_view.record_metadata rm
       ON li.bib_record_id = rm.id
     JOIN sierra_view.bib_record_property bp
       ON rm.id = bp.bib_record_id
-      --filter out digital records that holds can, but should not fall on
-      AND bp.material_code NOT IN ('b','y','s','h','w','l')
 
     WHERE
       (h.expires_gmt > CURRENT_DATE OR h.expires_gmt IS NULL)
       --limit results to just holds with a status of on hold
       AND h.status = '0'
+      --filter out digital records that holds can, but should not fall on
+      AND bp.material_code NOT IN ('b','y','s','h','w','l')
+      
+    /*
+	 Union to second query to account for bib level holds where there are no items or volumes present
+	 */  
+    UNION
+    
+    SELECT
+      DISTINCT h.id AS "holdID",
+      rm.record_type_code||rm.record_num AS "bibliographicRecordID",
+      h.placed_gmt AS "requestedDate",
+      /*logic for using first 3 characters of location code to designate branch specific to Minuteman*/
+      SUBSTRING(h.pickup_location_code,1,3) AS "requestedLocation",
+      CURRENT_DATE AS "reportDate"
+ 
+    FROM sierra_view.hold h
+    /*Using or in the join to reconcile both bib holds and item holds to a bib record*/
+    JOIN sierra_view.record_metadata rm
+      ON h.record_id = rm.id AND rm.record_type_code = 'b'
+    JOIN sierra_view.bib_record_property bp
+      ON rm.id = bp.bib_record_id
+
+    WHERE
+      (h.expires_gmt > CURRENT_DATE OR h.expires_gmt IS NULL)
+      --limit results to just holds with a status of on hold
+      AND h.status = '0'
+      --filter out digital records that holds can, but should not fall on
+      AND bp.material_code NOT IN ('b','y','s','h','w','l')    
 
     /*
-    Union to second query to account for volume level holds in addition to bib and item level ones
+    Union to third query to account for volume level holds in addition to bib and item level ones
     Minuteman does not use volume holds but meaningful for other customers
     */
 
@@ -525,13 +565,13 @@ def main():
       ON lv.bib_record_id = rm.id
     JOIN sierra_view.bib_record_property bp
       ON rm.id = bp.bib_record_id
-      --filter out digital records that holds can, but should not fall on
-      AND bp.material_code NOT IN ('b','y','s','h','w','l')
   
     WHERE  
       (h.expires_gmt > CURRENT_DATE OR h.expires_gmt IS NULL)
       --limit results to just holds with a status of on hold
       AND h.status = '0'
+      --filter out digital records that holds can, but should not fall on
+      AND bp.material_code NOT IN ('b','y','s','h','w','l')
     """
 
     on_order_query = """\
