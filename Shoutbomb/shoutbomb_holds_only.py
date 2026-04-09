5afb8cffd16caf5b971a7e047ef272ab3cd52849
Jeremy Goldstein <jgoldstein@minlib.net>
2026-04-09 14:27:49 -0400

    shoutbomb updates

--- Shoutbomb/shoutbomb_holds_only.py
+++ Shoutbomb/shoutbomb_holds_only.py
@@ -121,108 +121,18 @@ def main():
     WHERE h.status IN ('b','i')
 	  AND h.pickup_location_code IS NOT NULL
       AND h.pickup_location_code !~ '^(ca)|(do)|(le)'
-      AND NOW() - rmi.record_last_updated_gmt < INTERVAL '18 hours'
-    """
-
-    renewals_query = """
-    SELECT 
-      rmp.record_type_code||rmp.record_num||'a' AS patron_no,
-      REPLACE(i.barcode,' ','') AS item_barcode,
-      TRIM(TRAILING '/' FROM b.best_title) AS title,
-      TO_CHAR(c.due_gmt,'MM-DD-YYYY') AS due_date,
-      rmi.record_type_code||rmi.record_num ||'a' AS item_no,
-      ROUND(p.owed_amt,2) AS money_owed,
-      c.loanrule_code_num AS loan_rule,
-      NULLIF(COUNT(ih.id),0) AS item_holds,
-      NULLIF(COUNT(bh.id),0) AS bib_holds,
-      c.renewal_count AS renewals,
-      rmb.record_type_code||rmb.record_num||'a' AS bib_no,
-      c.id AS checkout_id
-  
-    FROM sierra_view.checkout c
-    JOIN sierra_view.patron_record p
-      ON c.patron_record_id = p.id
-    JOIN sierra_view.record_metadata rmp
-      ON p.id = rmp.id
-    JOIN sierra_view.item_record_property i
-      ON c.item_record_id = i.item_record_id
-    JOIN sierra_view.record_metadata rmi
-      ON i.item_record_id = rmi.id
-    JOIN sierra_view.bib_record_item_record_link l
-      ON i.item_record_id = l.item_record_id
-    JOIN sierra_view.bib_record_property b
-      ON l.bib_record_id = b.bib_record_id
-    JOIN sierra_view.record_metadata rmb
-      ON l.bib_record_id = rmb.id
-    LEFT JOIN sierra_view.hold bh
-      ON l.bib_record_id = bh.record_id 
-    LEFT JOIN sierra_view.hold ih
-      ON i.id = ih.record_id AND ih.status = '0'         
-
-    WHERE c.due_gmt::DATE - CURRENT_DATE BETWEEN 0 AND 3
-    GROUP BY 1,2,3,4,5,6,7,10,11,12
-    ORDER BY patron_no
-    """
-
-    overdues_query = """
-    SELECT 
-      rmp.record_type_code||rmp.record_num||'a' AS patron_no,
-      REPLACE(i.barcode,' ','') AS item_barcode,
-      TRIM(TRAILING '/' FROM b.best_title) AS title,
-      TO_CHAR(c.due_gmt,'MM-DD-YYYY') AS due_date,
-      rmi.record_type_code||rmi.record_num ||'a' AS item_no,
-      ROUND(p.owed_amt,2) AS money_owed,
-      c.loanrule_code_num AS loan_rule,
-      NULLIF(COUNT(ih.id),0) AS item_holds,
-      NULLIF(COUNT(bh.id),0) AS bib_holds,
-      c.renewal_count AS renewals,
-      rmb.record_type_code||rmb.record_num||'a' AS bib_no,
-      c.id AS checkout_id
-  
-    FROM sierra_view.checkout c
-    JOIN sierra_view.patron_record p
-      ON c.patron_record_id = p.id
-    JOIN sierra_view.record_metadata rmp
-      ON p.id = rmp.id
-    JOIN sierra_view.item_record_property i
-      ON c.item_record_id = i.item_record_id
-    JOIN sierra_view.record_metadata rmi
-      ON i.item_record_id = rmi.id
-    JOIN sierra_view.bib_record_item_record_link l
-      ON i.item_record_id = l.item_record_id
-    JOIN sierra_view.bib_record_property b
-      ON l.bib_record_id = b.bib_record_id
-    JOIN sierra_view.record_metadata rmb
-      ON l.bib_record_id = rmb.id
-    LEFT JOIN sierra_view.hold bh
-      ON l.bib_record_id = bh.record_id 
-    LEFT JOIN sierra_view.hold ih
-      ON i.id = ih.record_id AND ih.status = '0'        
-  
-    WHERE CURRENT_DATE - c.due_gmt::DATE BETWEEN 1 AND 30
-    GROUP BY 1,2,3,4,5,6,7,10,11,12
-    ORDER BY patron_no
+      AND NOW() - rmi.record_last_updated_gmt < INTERVAL '4 hours'
     """
 
     holds = run_query(holds_query)
-    renews = run_query(renewals_query)
-    overdues = run_query(overdues_query)
 
     holds_file_name = "/Scripts/Shoutbomb/Temp_Files/holds{}.txt".format(
         datetime.now().strftime("%Y%m%d-%H%M")
     )
-    renews_file_name = "/Scripts/Shoutbomb/Temp_Files/renew{}.txt".format(
-        datetime.now().strftime("%Y%m%d-%H%M")
-    )
-    overdues_file_name = "/Scripts/Shoutbomb/Temp_Files/overdue{}.txt".format(
-        datetime.now().strftime("%Y%m%d-%H%M")
-    )
 
     csv_writer(holds, holds_file_name)
-    csv_writer(renews, renews_file_name)
-    csv_writer(overdues, overdues_file_name)
 
-    os.system("c:\\Scripts\\Shoutbomb\\ftp_all.bat")
+    os.system("c:\\Scripts\\Shoutbomb\\ftp_holds.bat")
 
 
 # run main function and send error email to admin of script encounters an error
@@ -236,7 +146,7 @@ if __name__ == "__main__":
         emailto = config_recipient["script_error"]["recipients"].split()
 
         # craft email subject and message containing error message details from traceback
-        email_subject = "shoutbomb all script error"
+        email_subject = "shoutbomb holds only script error"
         email_message = (
             "Your script failed with the following error:\n\n" + traceback.format_exc()
         )
